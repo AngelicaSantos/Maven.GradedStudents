02a111df420ace7f9c5546423751c0377c3e7c2f
AngelicaSantos <angelica.santos2113@gmail.com>
2019-06-12 23:17:50 -0400

    complete

--- src/test/java/io/zipcoder/ClassroomTest.java
+++ src/test/java/io/zipcoder/ClassroomTest.java
@@ -1,4 +1,110 @@
 package io.zipcoder;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 public class ClassroomTest {
+    Student Angie;
+    Student Stephanie;
+    Student Kevin;
+    Student Emilio;
+    Student frank;
+    Student ted;
+    Student added;
+
+    Student null1;
+    Student null2;
+
+    Classroom classroom1;
+
+    ArrayList<Student> expectedA = new ArrayList<>();
+    ArrayList<Student> expectedB = new ArrayList<>();
+    ArrayList<Student> expectedC = new ArrayList<>();
+    ArrayList<Student> expectedD = new ArrayList<>();
+    ArrayList<Student> expectedF = new ArrayList<>();
+
+
+    @Before
+    public void setUp() {
+
+        Double[] oneExamScores = {10.0, 10.0, 20.0, 20.0, 10.0};
+        Angie = new Student("Angie", "Santos", oneExamScores);
+
+        Double[] twoExamScores = {20.0, 10.0, 30.0, 40.0, 40.0};
+        Stephanie = new Student("Stephanie", "Morel", twoExamScores);
+
+        Double[] threeExamScores = {20.0, 30.0, 40.0, 12.0, 30.0};
+        Kevin = new Student("Kevin", "Morel", threeExamScores);
+
+        Double[] fourExamScores = {40.0, 100.0, 100.0, 50.0, 100.0};
+        Emilio = new Student("Emilio", "Morel", fourExamScores);
+
+        Double[] fiveExamScores = {40.0, 100.0, 100.0, 50.0, 100.0};
+        frank = new Student("Frank", "Santos", fiveExamScores);
+
+        Double[] sixExamScores = {0.0, 0.0, 0.0, 0.0, 0.0};
+        ted = new Student("Ted", "Santos", sixExamScores);
+
+        Double[] addedExamScores = {20.0, 25.5, 45.0, 70.5, 10.0};
+        added = new Student("Added", "ToList", addedExamScores);
+
+
+        Student[] classStudents = {Angie, Stephanie, Kevin, Emilio, frank, ted};
+        classroom1 = new Classroom(classStudents);
+
+    }
+
+
+    @Test
+    public void getStudents() {
+        Student[] expected = {Angie, Stephanie, Kevin, Emilio, frank, ted};
+        Student[] actual = classroom1.getStudents();
+
+        Assert.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void getAverageExamScore() {
+        Double expected = 37.4;
+        Double actual = classroom1.getAverageExamScore();
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void addStudent() {
+        Student[] expected = {Angie, Stephanie, Kevin, Emilio, frank, ted, added};
+        classroom1.addStudent(added);
+
+        Student[] actual = classroom1.getStudents();
+
+        Assert.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void removeStudent() {
+        Student[] expected = {Angie, Stephanie, Kevin, Emilio, frank, ted};
+        classroom1.removeStudent("George", "Washington");
+        Student[] actual = classroom1.getStudents();
+
+        Assert.assertArrayEquals(expected, actual);
+    }
+
+
+    @Test
+    public void getStudentsByScore() {
+        Student[] expected = {Angie, Stephanie, Kevin, Emilio, frank, ted};
+        classroom1.getStudents();
+        Student[] actual = classroom1.getStudents();
+
+        Assert.assertArrayEquals(expected, actual);
+
+    }
 }
+
--- src/test/java/io/zipcoder/StudentTest.java
+++ src/test/java/io/zipcoder/StudentTest.java
@@ -1,5 +1,130 @@
 package io.zipcoder;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
 public class StudentTest {
+    Student Angelica;
+
+    @Before
+    public void setUp() {
+
+        Double[] examScores = {90.5, 100.0, 87.3, 120.5, 90.0};
+        Angelica = new Student("Angelica", "Santos", examScores);
+    }
+
+    @Test
+    public void getFirstName() {
+        String expected = "Angelica";
+        String actual = Angelica.getFirstName();
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void setFirstName() {
+        Angelica.setFirstName("Angelica");
+        String expected = "Angelica";
+        String actual = Angelica.getFirstName();
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void getLastName() {
+        String expected = "Santos";
+        String actual = Angelica.getLastName();
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void setLastName() {
+        Angelica.setLastName();
+        String expected = "Santos";
+        String actual = Angelica.getLastName();
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void getExamScores() {
+        String expected = "Exam Scores:\n" +
+                "       Exam 1 -> 90\n" +
+                "       Exam 2 -> 100\n" +
+                "       Exam 3 -> 87\n" +
+                "       Exam 4 -> 120\n" +
+                "       Exam 5 -> 90";
+        String actual = Angelica.getExamScores();
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void addExamScore() {
+        Angelica.addExamScore(101.5);
+        String expected = "Exam Scores:\n" +
+                "       Exam 1 -> 90\n" +
+                "       Exam 2 -> 100\n" +
+                "       Exam 3 -> 87\n" +
+                "       Exam 4 -> 120\n" +
+                "       Exam 5 -> 90\n" +
+                "       Exam 6 -> 101";
+        String actual = Angelica.getExamScores();
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void setExamScore() {
+
+        Angelica.setExamScore(2, 50.5);
+        String expected = "Exam Scores:\n" +
+                "       Exam 1 -> 90\n" +
+                "       Exam 2 -> 50\n" +
+                "       Exam 3 -> 87\n" +
+                "       Exam 4 -> 120\n" +
+                "       Exam 5 -> 90";
+        String actual = Angelica.getExamScores();
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void getAverageExamScore() {
+        Integer expected = 97;
+        Integer actual = Angelica.getAverageExamScore();
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void setExamScores() {
+        ArrayList<Double> newScores = new ArrayList<>();
+        newScores.add(10.5);
+        newScores.add(70.0);
+        newScores.add(87.3);
+        Angelica.setExamScores(newScores);
+
+        String expected = "Exam Scores:\n" +
+                "       Exam 1 -> 10\n" +
+                "       Exam 2 -> 70\n" +
+                "       Exam 3 -> 87";
+
+        String actual = Angelica.getExamScores();
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void getNumberOfExamsTaken() {
+        Integer expected = 5;
+        Integer actual = Angelica.getNumberOfExamsTaken();
+
+        Assert.assertEquals(expected, actual);
+    }
 
 }
